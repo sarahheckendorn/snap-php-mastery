bbdceae7797f528aa73f52e0b248500c9b151ef4
Sarah Heckendorn <praisinghim@jcangus.com>
2018-07-27 08:51:40 -0600

    added accessor methods and calculation function

--- index.php
+++ index.php
@@ -1,5 +1,28 @@
 <?php
 
-class Car{
+class Car {
+ private $carId;
+ private $carGas;
+ private $carMileage;
+
+ public function getCarId() : uuid {
+ 	return($this->carId);
+ }
+
+public function getCarGas() : string {
+ 	return($this->carGas)
+}
+
+	public function getCarMileage() : string {
+		return $this->carMileage;
+	}
+
+	public function addGasCost() {
+ 	$carGas=*3;
+ 	$carMilage=5;
+ 	$this->=$total;
+	}
+
+echo($total)
 
 }

3172c7bee005b7c297a8c22c2577e7276300c42a
Sarah Heckendorn <praisinghim@jcangus.com>
2018-07-27 09:13:44 -0600

    fixed parameters in addGasCost

--- index.php
+++ index.php
@@ -1,9 +1,9 @@
 <?php
 
 class Car {
- private $carId;
- private $carGas;
- private $carMileage;
+	private $carId;
+	private $carGas;
+	private $carMileage;
 
 
 	public function __construct(int $newCarId, string $newCarGas, string $newCarMileage) {
@@ -11,54 +11,60 @@ class Car {
 			$this->setCarId($newCarId);
 			$this->setCarGas($newCarGas);
 			$this->setCarMileage($newCarMileage);
-		} catch (\InvalidArgumentException | \RangeException | \Exception |\TypeError $exception) {
+		} catch(\InvalidArgumentException | \RangeException | \Exception |\TypeError $exception) {
 			$exceptionType = get_class($exception);
 			throw (new $exceptionType($exception->getMessage(), 0, $exception));
 		}
+	}
 
- public function getCarId() : uuid {
- 	return($this->carId);
- }
+	public function getCarId(): uuid {
+		return ($this->carId);
+	}
 
-	public function setCarId($newCarId) : void {
+	public function setCarId($newCarId): void {
 		$newCarId = trim($newCarId);
 		$newCarId = filter_var($newCarId, FILTER_SANITIZE_STRING, FILTER_FLAG_NO_ENCODE_QUOTES);
-		if(empty($newCarId) === true){
+		if(empty($newCarId) === true) {
 			throw(new \InvalidArgumentException("Field cannot be empty"));
 		}
 		$this->carId = $newCarId;
+	}
 
-public function getCarGas() : string {
- 	return($this->carGas);
+	public function getCarGas(): string {
+		return ($this->carGas);
+	}
+
+	public function setCarGas(string $newCarGas): void {
+		$newCarGas = trim($newCarGas);
+		$newCarGas = filter_var($newCarGas, FILTER_SANITIZE_STRING, FILTER_FLAG_NO_ENCODE_QUOTES);
+		if(empty($newCarGas) === true) {
+			throw(new \InvalidArgumentException("Field cannot be empty"));
+		}
+		$this->carGas = $newCarGas;
+	}
+
+	public function getCarMileage(): string {
+		return $this->carMileage;
+	}
+
+	public
+	function setCarMileage(string $newCarMileage): void {
+		$newCarMileage = trim($newCarMileage);
+		$newCarMileage = filter_var($newCarMileage, FILTER_SANITIZE_STRING, FILTER_FLAG_NO_ENCODE_QUOTES);
+		if(empty($newCarMileage) === true) {
+			throw(new \InvalidArgumentException("Field cannot be empty"));
+		}
+		$this->carMileage = $newCarMileage;
+
+	}
+
 }
 
-		public function setCarGas(string $newCarGas) : void {
-			$newCarGas = trim($newCarGas);
-			$newCarGas = filter_var($newCarGas, FILTER_SANITIZE_STRING, FILTER_FLAG_NO_ENCODE_QUOTES);
-			if(empty($newCarGas) === true) {
-				throw(new \InvalidArgumentException("Field cannot be empty"));
-			}
-			$this->carGas = $newCarGas;
-
-			public
-			function getCarMileage(): string {
-				return $this->carMileage;
-			}
-
-			public
-			function setCarMileage(string $newCarMileage): void {
-				$newCarMileage = trim($newCarMileage);
-				$newCarMileage = filter_var($newCarMileage, FILTER_SANITIZE_STRING, FILTER_FLAG_NO_ENCODE_QUOTES);
-				if(empty($newCarMileage) === true) {
-					throw(new \InvalidArgumentException("Field cannot be empty"));
-				}
-				$this->carMileage = $newCarMileage;
-
-			}
-		}
-		public function addGasCost() {
-			$carGas= *=3;
- 			$carMilage=5;
- 			$total=$carGas + $carMilage;
- 	$this->addGasCost()
+		public function addGasCost($carGas, $carMileage) {
+			$carGas *= 3;
+ 			$carMileage=5;
+ 			$total=$carGas + $carMileage;
+ 			$this->addGasCost()
 	}
+
+	echo($this->$total);
